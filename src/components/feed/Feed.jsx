b3d84f0314ebd207aa98f49ae057ed8a74b6d69a
Manuel Santos <manueljoaosantos@gmail.com>
2024-01-22 12:24:50 +0000

    feat: finished layout

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,9 +1,20 @@
+import Share from "../share/Share"
+import Post from "../post/Post"
 import "./feed.css"
 
 export default function Feed() {
   return (
     <div className="feed">
-    <div className="feedWrapper">Feed</div>
+    <div className="feedWrapper">
+      <>
+      <Share/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>                        
+      </>
+    </div>
     </div>
   )
 }
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,37 @@ export default function Rightbar() {
   return (
 <div className="rightbar">
       <div className="rightbarWrapper">
-      Rightbar
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Manuel Teste</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Manuel Teste</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Manuel Teste</span>
+          </li>                    
+        </ul>
       </div>
     </div>
   )
